5e7ce36253cc997e60abcfba12a6d2895584d37f
kanokwan <kanokwan.tangtastham@gmail.com>
2021-07-14 14:47:08 +0700

    test

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { store } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { NativeBaseProvider } from "native-base";
-import { createStackNavigator } from "@react-navigation/stack";
 import configureStore from "./store/rootStore";
 import AppNavigation from "./navigation/appNavigation";
 
-const Stack = createStackNavigator();
-
 export default function App() {
-  const { store } = configureStore();
+  const { store, persistor } = configureStore();
 
   return (
     <Provider store={store}>
-      <NativeBaseProvider>
-        <AppNavigation />
-      </NativeBaseProvider>
+      <PersistGate persistor={persistor}>
+        <NativeBaseProvider>
+          <AppNavigation />
+        </NativeBaseProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- screens/login/login.js
+++ screens/login/login.js
@@ -9,7 +9,16 @@ import {
   Alert,
   Stack,
 } from "native-base";
+import {
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  ScrollView,
+  View,
+} from "react-native";
 import userAction from "../../store/user/userActions";
+import { style } from "styled-system";
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -37,43 +46,65 @@ const Login = () => {
   const handleChangePassword = (event) => setValuePassword(event.target.value);
 
   return (
-    <Container>
-      {showAlert ? (
-        <Stack space={4} mx={3} w="100%">
-          <Alert variant="left-accent" status="warning">
-            <Alert.Icon />
-            <Alert.Title flexShrink={1}>
-              Usrname และ Password ต้องไม่เป็นค่าวาง
-            </Alert.Title>
-          </Alert>
-        </Stack>
-      ) : null}
-      <VStack space={4}>
-        <Box>
-          <Input
-            size="md"
-            w="100%"
-            placeholder="usrname"
-            value={valueUsername}
-            onChange={handleChangeUsername}
-          />
-        </Box>
-        <Box>
-          <Input
-            size="md"
-            w="100%"
-            type="password"
-            placeholder="password"
-            value={valuePassword}
-            onChange={handleChangePassword}
-          />
-        </Box>
-        <Box>
-          <Button onPress={login}>LOGIN</Button>
-        </Box>
-      </VStack>
-    </Container>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {showAlert ? (
+          <Stack space={4} mx={3} w="100%">
+            <Alert variant="left-accent" status="warning">
+              <Alert.Icon />
+              <Alert.Title flexShrink={1}>
+                Usrname และ Password ต้องไม่เป็นค่าวาง
+              </Alert.Title>
+            </Alert>
+          </Stack>
+        ) : null}
+        <VStack space={4}>
+          <View style={styles.containerLogo}>
+            <Image
+              source={require("../../assets/images/react.png")}
+              style={{ width: 200, height: 200 }}
+            />
+          </View>
+          <Box>
+            <Input
+              size="md"
+              w="100%"
+              placeholder="usrname"
+              value={valueUsername}
+              onChange={handleChangeUsername}
+            />
+          </Box>
+          <Box>
+            <Input
+              size="md"
+              w="100%"
+              type="password"
+              placeholder="password"
+              value={valuePassword}
+              onChange={handleChangePassword}
+            />
+          </Box>
+          <Box>
+            <Button onPress={login}>LOGIN</Button>
+          </Box>
+        </VStack>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    marginHorizontal: 20,
+  },
+  containerLogo: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Login;
